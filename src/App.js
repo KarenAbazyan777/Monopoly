9b47a9cc86693602abac083f7d685fe0db6de765
Karen Abazyan <abazyankaren09@gmail.com>
2023-12-04 12:58:28 +0400

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,179 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ 
+  return(
+      
+      <div className="main">
+          <div className='full-lines'>
+              <div className="firstl-1item">
+                Roll the dice again 
+                <span className="dicearrow">
+                    &#10155;
+                </span>
+              </div>
+              <div className="firstl-2item"><b>300</b></div>
+              <div className="firstl-3item"><b>300</b></div>
+              <div className="firstl-4item"><b>400</b></div>
+              <div className="firstl-5item"><b>400</b></div>
+              <div className="firstl-6item"><b>100</b></div>
+              <div className="firstl-7item"><span>Chance</span></div>
+              <div className="firstl-8item"><b>400</b></div>
+              <div className="firstl-9item"><b>450</b></div>
+              <div className="firstl-10item"><b>450</b></div>
+              <div className="firstl-11item">
+              <b>Go to police</b>
+              <span>&#8665;</span>
+              </div>
+          </div>
+          <div className='full-lines'>
+              <div className="secondl-1item"><b>300</b></div>
+              <div className="secondl-2item"><b>2000</b></div>
+              <div className="secondl-3item"><b>2500</b></div>
+              <div className="secondl-4item"><b>&#9839;</b></div>
+              <div className="secondl-5item"><b>&#9839;</b></div>
+              <div className="secondl-6item"><b>3000</b></div>
+              <div className="secondl-7item"><b>&#9839;</b></div>
+              <div className="secondl-8item"><b>&#9839;</b></div>
+              <div className="secondl-9item"><b>&#9839;</b></div>
+              <div className="secondl-10item"><b>5000</b></div>
+              <div className="secondl-11item"><b>500</b></div>
+          </div>
+          <div className='full-lines'>
+              <div className="thirdl-1item"><b>300</b></div>
+              <div className="thirdl-2item"><b>&#9839;</b></div>
+              <div className="thirdl-3item"><b>2000</b></div>
+              <div className="thirdl-4item"><b>2000</b></div>
+              <div className="thirdl-5item"><b>2000</b></div>
+              <div className="thirdl-6item"><b>2500</b></div>
+              <div className="thirdl-7item"><b>2500</b></div>
+              <div className="thirdl-8item"><span>Chance</span></div>
+              <div className="thirdl-9item"><b>2500</b></div>
+              <div className="thirdl-10item">
+                <b>
+                    &#9839;
+                </b>
+                <span className='arrowOfSharp'>
+                    &#10155;
+                </span>
+              </div>
+              <div className="thirdl-11item"><b>500</b></div>
+          </div>
+          <div className='center'>
+            <div className='item t34'><b>200</b></div>
+            <div className='item t35'><b>&#9839;</b></div>
+            <div className='item t36'><b>1500</b></div>
+            <div className='item t37'>
+                <h1>MONOPOLY</h1>
+            </div>
+            <div className='item t38'><b>2500</b></div>
+            <div className='item t39'><b>600</b></div>
+            <div className='item t40'><b>200</b></div>
+            <div className='item t41'><b>1500</b></div>
+            <div className='item t42'><b>1500</b></div>
+            <div className='item t43'><b>1000</b></div>            
+            <div className='item t44'><b>800</b></div>
+            <div className='item t45'>
+                <span>
+                    Chance
+                </span>
+                <div className='arrowOfChance'>
+                    &#10155;
+                </div>
+            </div>
+            <div className='item t46'><b>1000</b></div>
+            <div className='item t47'><b>1500</b></div>
+            <div className='item t48'><b>3000</b></div>
+            <div className='item t49'>
+                 <div className='leftarrow'>&#10155;</div>
+                 <h3>Chance</h3>
+                 <div className='rightarrow'>&#10155;</div>
+            </div>
+            <div className='item t50'><b>500</b></div>
+            <div className='item t51'><b>200</b></div>
+            <div className='item t52'><b>1000</b></div>
+            <div className='item t53'><b>3000</b></div>
+            <div className='item t54'><b>400</b></div>
+            <div className='item t55'><b>100</b></div>
+            <div className='item t56'><b>1000</b></div>
+            <div className='item t57'><b>3000</b></div>
+            <div className='item t58'><b>800</b></div>
+          </div>
+          {/* <div className='fourth-line'>
+              <div  className='item1-fl'></div>
+              <div  className='item2-fl'></div>
+              <div  className='item3-fl' ></div>
+              <div  className='monopoly-item'>MONOPOLY</div>
+              <div  className='item4-fl'></div>
+              <div  className='item5-fl'></div>
+              
+          </div>
+          <div className='fifth-line'>
+              <div  className='item1-fivel'></div>
+              <div  className='item2-fivel'></div>
+              <div  className='item3-fivel' ></div>
+              <div  className='item4-fivel'></div>
+              <div  className='item5-fivel'></div>
+          </div>
+          <div className='sixth-line'>
+              <div className='item1-sixl'></div>
+              <div className='item2-sixl'></div>
+              <div className='item3-sixl'></div>
+              <div className='item4-sixl'></div>
+              <div className='item5-sixl'></div>
+              <div className='item6-sixl'></div>
+          </div>
+          <div className='seventh-line'>
+              <div className='item1-sl'></div>
+              <div className='item2-sl'></div>
+              <div className='item3-sl'></div>
+              <div className='item4-sl'></div>
+          </div>
+          </div>
+          <div className='eighth-line'>
+              <div className='item1-el'></div>
+              <div className='item2-el'></div>
+              <div className='item3-el'></div>
+              <div className='item4-el'></div>
+          </div> */}
+          <div className='nineth-line'>
+              <div className='item1-nl'><b>200</b></div>
+              <div className='item2-nl'><b>1000</b></div>
+              <div className='item3-nl'><b>5000</b></div>
+              <div className='item4-nl'><b>5000</b></div>
+              <div className='item5-nl'><b>4000</b></div>
+              <div className='item6-nl'><b>3500</b></div>
+              <div className='item7-nl'><span>Chance</span></div>
+              <div className='item8-nl'><b>3500</b></div>
+              <div className='item9-nl'><b>900</b></div>
+          </div>
+          <div className='tenth-line'>
+              <div className='item1-tl'><b>100</b></div>
+              <div className='item2-tl'>
+                 <div className='toparrow'>&#8687;</div>
+                 <h3>Chance</h3>
+                 <div className='bottomarrow'>&#8687;</div>
+              </div>
+              <div className='item3-tl'><b>100</b></div>
+          </div>
+          <div className='full-lines'>
+              <div className="elevenl-1item"></div>
+              <div className="elevenl-2item"><b>200</b></div>
+              <div className="elevenl-3item"><b>140</b></div>
+              <div className="elevenl-4item"><b>120</b></div>
+              <div className="elevenl-5item"><b>100</b></div>
+              <div className="elevenl-6item"><b>100</b></div>
+              <div className="elevenl-7item"><b>100</b></div>
+              <div className="elevenl-8item"><b>80</b></div>
+              <div className="elevenl-9item"><b>60</b></div>
+              <div className="elevenl-10item"><span>Chance</span></div>
+              <div className="elevenl-11item"></div>
+          </div>
+         
+         
+        </div>
+  )
+
 }
 
 export default App;
